c4fcb9a69dd52b5e5713a257d88fc9dd8beffc3a
Emaan Hookey <em.grey28@gmail.com>
2025-08-27 12:27:25 -0230

    added missed files

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1749,6 +1749,21 @@ describe('', () => {
         });
 
         it('should cancel if title was not changed', () => {
+          const spy = cy.stub().callsFake(req => req.reply({ body: { ...req.body, id: 257334 } })).as('renameCallback');
+          page.mockUpdate(257334, spy);
+
+          todos.title(0).trigger('dblclick');
+          todos.titleField(0).should('exist').then($input => {
+            $input[0].focus();
+            $input[0].blur();
+          });
+          cy.get('@renameCallback').should('not.be.called');
+          page.flushJSTimers();
+          todos.titleField(0).should('not.exist');
+          todos.assertTitle(0, 'HTML');
+        });
+
+        it('should cancel if title was not changed', () => {
           const spy = cy.stub()
             .callsFake(req => req.reply({ body: { ...req.body, id: 257334 } }))
             .as('renameCallback');
